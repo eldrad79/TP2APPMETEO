d358efb12c4ad4375b4f28941a8435d9a453afbe
eldrad79 <65157202+eldrad79@users.noreply.github.com>
2021-05-17 18:19:44 +0200

    ajout de la maquette.drawio et debut de JS

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,5 @@
-const urlb = `api.openweathermap.org/data/2.5/weather?q=${ville}&appid=bc9752ae85bb91066c7caa0ae2b4c27d`
+// const urlb = `api.openweathermap.org/data/2.5/weather?q=${ville}&appid=bc9752ae85bb91066c7caa0ae2b4c27d`
+
+const inRecherche = document.getElementsByClassName('.inputRecherche').length
+
+console.log(inRecherche)
