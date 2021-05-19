96c5baf14975be51c36b7848de4e105fa69a500b
eldrad79 <65157202+eldrad79@users.noreply.github.com>
2021-05-19 10:12:47 +0200

    formatage et ajout README

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,2 @@
-// const urlb = `api.openweathermap.org/data/2.5/weather?q=${ville}&appid=bc9752ae85bb91066c7caa0ae2b4c27d`
-
-const inRecherche = document.getElementsByClassName('.inputRecherche').length
-
-console.log(inRecherche)
+/* global appmeteo */
+const appmeteo = document.querySelector('input').addEventListener('input', function () {})
---
+++ README.md
@@ -0,0 +1,3 @@
+# TP2 Météo 
+Création d'une app météo avec openWeatherMap
+[![JavaScript Style Guide](https://cdn.rawgit.com/standard/standard/master/badge.svg)](https://github.com/standard/standard)
