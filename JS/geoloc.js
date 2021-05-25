910efb3750309dd21cf7e1e47cc0e9365e747788
eldrad79 <65157202+eldrad79@users.noreply.github.com>
2021-05-25 17:01:37 +0200

    ajout des fonctionnalitees de l'input

--- JS/geoloc.js
+++ JS/geoloc.js
@@ -8,9 +8,26 @@ const ville = await fetch(`http://api.ipapi.com/api/${ip}?access_key=5c745a63453
     .then(Resultat => Resultat.json())
     .then(json =>json.city)
 // 3 Choper les informations meteo grace a la ville : http://api.openweathermap.org/data/2.5/weather?q=${ville}&units=metric&appid=bc9752ae85bb91066c7caa0ae2b4c27d
-const meteo = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ville}&units=metric&appid=bc9752ae85bb91066c7caa0ae2b4c27d`)
+const meteo = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ville}&lang=fr&units=metric&appid=bc9752ae85bb91066c7caa0ae2b4c27d`)
 .then(Resultat => Resultat.json())
 .then(json => json) 
+
+    let affichage = ville + " " + meteo.weather[0].description;
+    document.querySelector("#villes").innerHTML = affichage;
+
 }
+geoloc()
 // 4 afficher le resultat.
-geoloc();
+
+const btn = document.querySelector('button');
+async function recherche () {
+    const ville = document.querySelector('input').value;
+    const meteo = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ville}&units=metric&lang=fr&appid=bc9752ae85bb91066c7caa0ae2b4c27d`)
+    .then(Response=> Response.json())
+    .then(json => json)
+    
+    let affichage = ville + " " + meteo.weather[0].description;
+    document.querySelector("#villes").innerHTML = affichage;
+}
+btn.addEventListener('click' ,recherche);
+document.querySelector('span').addEventListener('click', geoloc);
